6735d33d819a7e3120f560970fef364e6be30980
VictoriaEFernandez <vicki311fdz@gmail.com>
2024-09-17 16:45:09 -0300

    mas ejercicicios

--- javascript.js
+++ javascript.js
@@ -40,3 +40,51 @@ function calcularTotalPagar() {
 
     alert("El total a abonar por el comprador es: " + total);
 }
+//5. Funcion con uso de variables
+function Variables(){
+    let nombre = prompt("Ingresa el nombre: ");
+    let sueldo = parseInt(prompt("Ingresa el sueldo: "));
+    document.write("Hola " + nombre);
+    document.write("Tu sueldo es de: " + sueldo)
+}
+//6. Funcion para ver el promedio de Notas
+function notasAlumno(){
+    let nota1 = parseInt(prompt("Ingrese nota 1: "));
+    let nota2 = parseInt(prompt("Ingrese nota 2: "));
+    let nota3 = parseInt(prompt("Ingrese nota 3: "));
+    let promedio = (nota1 + nota2 + nota3 ) /3;
+    if (promedio >= 7){
+        document.write('Promocionado')
+    }
+    else{
+        document.write('No promocionaste')
+    }
+
+
+}
+//7. Funcion para ver si dos claves son iguales
+function obtenerClavesIguales(){
+    let clave1 = prompt("Ingrese la clave 1: ");
+    let clave2 = prompt("Ingrese la clave 2: ");
+    if (clave1 === clave2){
+        document.write("Las claves son iguales")
+    }
+    else{
+        alert('Prosiga');
+    }
+}
+//8.Funcion operaciones [Estrcuturas condicionales compuestas]
+function operacionesEstructura(){
+    let valor1 = parseInt(prompt("Ingrese el primer valor: "));
+    let valor2 = parseInt(prompt("Ingrese el segundo valor: "));
+    if(valor1 > valor2){
+        let suma = valor1 + valor2;
+        let resta = valor1 - valor2;
+        alert("La suma de los valores es: "+ suma + ", y la resta es: "+ resta)
+    }
+    else{
+        let producto = valor1 * valor2;
+        let cociente = valor1 / valor2;
+        alert("El producto es: " + producto + ", y el cociente del primero respecto al segundo es: " + cociente);
+    }
+}
